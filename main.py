8398dbd3f0a887017471ccedf1295c060dc5a65b
Ely6899 <elikozinetz@protonmail.com>
2024-11-11 16:09:34 +0200

    Encoder logic added and project requirements updated

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from GeneralUtils.DirPaths import LIBRISPEECH_TRAIN_ROOT_FOLDER
 from GeneralUtils.FileUtils import get_intermediate_folders
 
+import torch
+
 if __name__ == '__main__':
-    print(get_intermediate_folders(Path("LibriSpeech/19/198/19-198-0000.flac"), LIBRISPEECH_TRAIN_ROOT_FOLDER))
+    print(torch.cuda.is_available())
 
