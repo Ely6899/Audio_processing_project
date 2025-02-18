c706b4af850e4a3dd524bb4e4f8463c2ec362a7c
נועם סיידה <noamsayada1@gmail.com>
2025-02-18 22:43:36 +0200

    restoring hop_len to n_fft//2 to train the model

--- Models/SentimentAnalysis/PreprocessParams.py
+++ Models/SentimentAnalysis/PreprocessParams.py
@@ -3,7 +3,7 @@ SAMPLE_RATE = 16000
 
 N_FFT = 512
 WINDOW_LENGTH = N_FFT
-HOP_LENGTH = N_FFT // 4
+HOP_LENGTH = N_FFT // 2
 MAX_SPECTOGRAM_DURATION_IN_SECONDS = 4.5
 
 #Calculate the max number of samples for the target duration
@@ -12,3 +12,5 @@ MAX_SAMPLES = int(MAX_SPECTOGRAM_DURATION_IN_SECONDS * SAMPLE_RATE)
 # Calculate the target number of frames for the spectrogram
 #NOTE: For now, needs to be divisible by 8.
 TARGET_FRAMES = (MAX_SAMPLES - WINDOW_LENGTH) // HOP_LENGTH + 1 # noam: this is the right formula for the number of frames
+
+# print(TARGET_FRAMES) # debug
