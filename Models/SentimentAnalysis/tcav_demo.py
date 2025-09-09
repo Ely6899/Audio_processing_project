42ceab12fe4fb115892f089b5bcc0031b257194e
Noam Sayada <noamsayada1@gmail.com>
2025-09-09 09:02:55 +0300

    modified per-sample tcav to save sample's path

--- Models/SentimentAnalysis/tcav_demo.py
+++ Models/SentimentAnalysis/tcav_demo.py
@@ -1,3 +1,4 @@
+from xml.parsers.expat import model
 from IPython.display import display
 
 from pathlib import Path
@@ -5,23 +6,48 @@ from pathlib import Path
 # from functorch.dim import Tensor #! makes a bug because functorch.dim isn't supported in python 3.12 !!
 from pprint import pprint
 from typing import Optional
-from tqdm import tqdm
 
-import csv
 import numpy as np
 import pandas as pd
 import torch
 from captum.concept import TCAV, Concept
 from torch.utils.data import DataLoader, Dataset
+import tqdm
 
 from Preprocess import audio_to_mel_spectrogram
 from PreprocessParams import TARGET_FRAMES, FREQUENCY_BIN_COUNT
 from concepts_creation import generate_random_pattern_spectrogram
-
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device="cpu"
+from tqdm import tqdm
 
 
+
+CONCEPT_UNIQUE_NAMES = [
+                        "long_constant_thick",
+                        "long_dropping_flat_thick",
+                        "long_dropping_steep_thick",
+                        "long_dropping_steep_thin",
+                        "long_rising_flat_thick",
+                        "long_rising_steep_thick",
+                        "long_rising_steep_thin",
+                        "short_constant_thick",
+                        "short_dropping_steep_thick",
+                        "short_dropping_steep_thin",
+                        "short_rising_steep_thick",
+                        "short_rising_steep_thin"
+                        ]
+
+INDEX_EMOTION_MAPPING = {
+    '01': 'neutral', '02': 'calm', '03': 'happy', '04': 'sad',
+    '05': 'angry', '06': 'fearful', '07': 'disgust', '08': 'surprised',
+}
+
+LABEL_EMOTION_MAPPING = {
+    0: 'angry', 1: 'calm', 2: 'disgust', 3: 'fearful',
+    4: 'happy', 5: 'neutral', 6: 'sad', 7: 'surprised',
+}
+
+
+# PyTorch Datasets for TCAV
 
 class PreGeneratedRandomSpectrogramDataset(Dataset):
     """
@@ -37,7 +63,7 @@ class PreGeneratedRandomSpectrogramDataset(Dataset):
         # Pre-generate all spectrograms in memory
         self.data = np.array([generate_random_pattern_spectrogram(freq_count, frames, rng=self.rng)
                      for _ in range(n_samples)])
-        self.data = torch.tensor(self.data, dtype=torch.float32).to(device=device)
+        self.data = torch.tensor(self.data, dtype=torch.float32)
 
     def __len__(self):
         return len(self.data)
@@ -67,11 +93,11 @@ class PreGeneratedConceptDataset(Dataset):
         # load all .npy files from root_concept_dir/concept_name
         self.data = []
         concept_dir = self.root_concept_dir / self.concept_name
-        concept_dir.mkdir(exist_ok=True, parents=True)
+        concept_dir.mkdir(exist_ok=True)
         for npy_file in concept_dir.glob("*.npy"):
             self.data.append(np.load(npy_file))
         self.data = np.array(self.data)
-        self.data = torch.tensor(self.data, dtype=torch.float32).to(device=device)
+        self.data = torch.tensor(self.data, dtype=torch.float32)
 
     def __len__(self):
         return len(self.data)
@@ -85,66 +111,15 @@ class PreGeneratedConceptDataset(Dataset):
     def get_data(self):
         return self.data
 
-concept_unique_names = [
-                        "long_constant_thick",
-                        "long_dropping_flat_thick",
-                        "long_dropping_steep_thick",
-                        "long_dropping_steep_thin",
-                        "long_rising_flat_thick",
-                        "long_rising_steep_thick",
-                        "long_rising_steep_thin",
-                        "short_constant_thick",
-                        "short_dropping_steep_thick",
-                        "short_dropping_steep_thin",
-                        "short_rising_steep_thick",
-                        "short_rising_steep_thin"
-                        ]
+# Functions
 
-index_emotion_mapping = {
-    '01': 'neutral', '02': 'calm', '03': 'happy', '04': 'sad',
-    '05': 'angry', '06': 'fearful', '07': 'disgust', '08': 'surprised',
-}
-
-label_emotion_mapping = {
-    0: 'angry', 1: 'calm', 2: 'disgust', 3: 'fearful',
-    4: 'happy', 5: 'neutral', 6: 'sad', 7: 'surprised',
-}
-
-def get_emotion_tensor(emotion_label: str, drop_false_positive: bool) -> torch.Tensor:
-    """
-    return emotion tensor containing all the spectrograms that the model predicted as "emotion_label"
-
-    Args:
-        emotion_label (str): The emotion label to filter by.
-        drop_false_positive (bool): e.g. if emotion_label='angry', then audio classified as 'angry' but not actually 'angry' will be dropped.
-
-    Returns:
-        torch.Tensor: A tensor containing the spectrograms for the specified emotion label. in shape: [Batch, 1, Height, Width]
-    """
-    df = pd.read_csv("attributes/all_attributes.csv",usecols=['path', 'predicted_label', 'true_label'], quoting=csv.QUOTE_NONE,
-                     encoding='utf-8', engine='python', dtype=str)
-
-    df_emotion = df[(df["predicted_label"] == emotion_label) & (df["true_label"] == emotion_label)]["path"] if drop_false_positive else df[(df["predicted_label"] == emotion_label)]["path"]
-    
-    # audio_to_mel_spectrogram to all audio samples
-    mel_specs = [audio_to_mel_spectrogram(Path(path.replace("\\", "/").strip())) for path in df_emotion]
-
-    # stack all the mel spectrograms to one big tensor
-    emotion_tensor = torch.stack([torch.tensor(spec) for spec in mel_specs])
-    
-    # if shape=[B,H,W] change it to [B,1,H,W]
-    if emotion_tensor.dim() == 3:
-        emotion_tensor = emotion_tensor.unsqueeze(1)  # [B, 1, H, W]
-        
-    return emotion_tensor.to(device=device)
-
-def tcav_scores_to_df(scores_by_label: dict, concept_names: list[str]) -> pd.DataFrame:
-    """
-    Flatten Captum TCAV results into a DataFrame with:
-    columns = ["label_name", "concept_name", "layer_name", "positive_sign_count", "positive_magnitude"]
-    """
+def _tcav_dict_per_sample_to_df(scores_by_sample: dict, concept_names: list[str]) -> pd.DataFrame:
+    # """
+    # Flatten Captum TCAV results into a DataFrame with:
+    # columns = ["label_name", "concept_name", "layer_name", "positive_percentage", "magnitude"]
+    # """
     rows = []
-    for label_name, exp_sets in scores_by_label.items():
+    for path, exp_sets in scores_by_sample.items():
         # exp_key looks like "0-12" where 0 is the positive concept index, 12 is random/baseline
         for exp_key, layer_dict in exp_sets.items():
             try:
@@ -170,87 +145,96 @@ def tcav_scores_to_df(scores_by_label: dict, concept_names: list[str]) -> pd.Dat
 
                 # Positive direction = index 0
                 rows.append({
-                    "label_name": label_name,
+                    "path": path,
                     "concept_name": concept_name,
                     "layer_name": layer_name,
-                    "positive_sign_count": float(sc[0]),
-                    "positive_magnitude": float(mg[0]),
+                    "positive_percentage": float(sc[0]),
+                    "magnitude": float(mg[0]),
                 })
 
     return pd.DataFrame(rows, columns=[
-        "label_name", "concept_name", "layer_name", "positive_sign_count", "positive_magnitude"
+        "path", "concept_name", "layer_name", "positive_percentage", "magnitude"
     ])
 
-# store spectrograms of each emotion in Tensor object.
-label_inputs = {
-    label_name: get_emotion_tensor(label_name, drop_false_positive=True)
-    for label_name in label_emotion_mapping.values()
-}
+# all_filtered_data is for droping men samples and/or false positive samples 
+def _get_tcav_dict_per_sample(all_filtered_data: pd.DataFrame): 
+    
+    # -----------------------------
+    # 1️⃣ Load pretrained model
+    # -----------------------------
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.load(Path("ResNetWithAttention.pt"), map_location=device, weights_only=False)
+    model.eval()
+
+    # -----------------------------
+    # 2️⃣ Choose layer for TCAV to work on
+    # -----------------------------
+
+    layer = "module3.blocks.0.conv2"
+
+    # -----------------------------
+    # 3️⃣ Compute TCAV
+    # -----------------------------
+    # Captum TCAV expects a dictionary of concept activations, with positive and negative examples.
 
 
-# -----------------------------
-# 1️⃣ Load pretrained model
-# -----------------------------
-model = torch.load(Path("ResNetWithAttention.pt"), map_location=device, weights_only=False)
-model.eval()
-
-# -----------------------------
-# 2️⃣ Choose layer for TCAV to work on
-# -----------------------------
-
-layer = "module3.blocks.0.conv2"
-
-# -----------------------------
-# 3️⃣ Compute TCAV
-# -----------------------------
-# Captum TCAV expects a dictionary of concept activations, with positive and negative examples.
+    # Define TCAV object
+    tcav = TCAV(model, [layer])
 
 
-# Define TCAV object
-tcav = TCAV(model, [layer])
-tcav_scores_per_label= {}
+    positive_concepts: list[Concept] = [Concept(id=concept_idx, name=concept_name, data_iter=DataLoader(PreGeneratedConceptDataset(n_samples=100, concept_name=concept_name), shuffle=False))
+                                for concept_idx, concept_name in enumerate(CONCEPT_UNIQUE_NAMES)]
 
+    # This concept is the negative of concepts.
+    negative_concept_dataset = PreGeneratedRandomSpectrogramDataset(n_samples=100, freq_count=FREQUENCY_BIN_COUNT, frames=TARGET_FRAMES)
+    random_concept = Concept(id=len(positive_concepts), name='random', data_iter=DataLoader(negative_concept_dataset, shuffle=False))
 
-positive_concepts: list[Concept] = [Concept(id=concept_idx, name=concept_name, data_iter=DataLoader(PreGeneratedConceptDataset(n_samples=60, concept_name=concept_name), shuffle=False))
-                               for concept_idx, concept_name in enumerate(concept_unique_names)]
+    # Debug call, don't uncomment
+    # show_arrays_in_separate_windows(negative_concept_dataset.get_data)
 
-# This concept is the negative of concepts.
-negative_concept_dataset = PreGeneratedRandomSpectrogramDataset(n_samples=10, freq_count=FREQUENCY_BIN_COUNT, frames=TARGET_FRAMES)
-random_concept = Concept(id=len(positive_concepts), name='random', data_iter=DataLoader(negative_concept_dataset, shuffle=False))
+    print("Reached tcav interpret")
+    
+    tcav_dict_per_sample = {}
+    
+    # for row(pandas series) in df:
+    for i, row in tqdm(all_filtered_data.iterrows(), total=len(all_filtered_data), desc="Processing samples"):
+        label_name = row['predicted_label']
+        path = row['path']
+        sample = torch.tensor(audio_to_mel_spectrogram(Path(path)), dtype=torch.float32).unsqueeze(0).unsqueeze(0)  # shape [1, 1, H, W]
+        label_index = list(LABEL_EMOTION_MAPPING.keys())[list(LABEL_EMOTION_MAPPING.values()).index(label_name)]
+        tcav_dict_per_sample[path] = {}
+        
+        score_for_label = tcav.interpret(
+                inputs=sample,
+                experimental_sets=[[c, random_concept] for c in positive_concepts],
+                target=label_index
+            )
+        
+        tcav_dict_per_sample[path] = score_for_label
+        
+    
+    return tcav_dict_per_sample
 
-# Debug call, don't uncomment
-# show_arrays_in_separate_windows(negative_concept_dataset.get_data)
+def get_tcav_per_sample():
+    df_attributes = pd.read_csv("attributes/all_attributes.csv")
 
-# -----------------------------
-# 3️⃣b Compute TCAV per-sample
-# -----------------------------
-tcav_scores_per_sample = {}
+    # ## !debug:
+    # df_attributes = df_attributes.head(10)
+    # ## !debug
+    
 
-for label_index, label_name in label_emotion_mapping.items():
-    samples = label_inputs[label_name]
-    tcav_scores_per_sample[label_name] = []
+    # drop unnecessary columns
+    df_attributes.drop(columns=[f'prob {emotion}' for emotion in LABEL_EMOTION_MAPPING.values()], inplace=True)
 
-    for i in tqdm(range(samples.shape[0]), f"Calculating on {label_name}"):  # iterate over each sample
-        single_input = samples[i].unsqueeze(0)  # [1, 1, H, W]
+    dic = _get_tcav_dict_per_sample(df_attributes)
+    
+    df_tcav = _tcav_dict_per_sample_to_df(dic, CONCEPT_UNIQUE_NAMES)
+    
+    # create a new df, which is df_tcav but added attributes from df_attributes based on the 'path' column
+    df_merged = df_tcav.merge(df_attributes, on='path', how='left')
 
-        scores = tcav.interpret(
-            inputs=single_input,
-            experimental_sets=[[c, random_concept] for c in positive_concepts],
-            target=label_index
-        )
-
-        tcav_scores_per_sample[label_name].append(scores)
-
-# Now tcav_scores_per_sample[label_name][i] contains the TCAV results for the i-th sample.
-
-# -----------------------------
-# 4️⃣ Inspect results
-# -----------------------------
-
-#pprint(tcav_scores_per_sample)
-
-# pprint(f"TCAV scores for label {'angry'}: {tcav_scores_per_label['angry']}", depth=1)
-
-df_tcav = tcav_scores_to_df(tcav_scores_per_label, concept_unique_names)
-
-display(df_tcav)
+    # rearrange columns in a custom order
+    desired_order = ['path', 'true_label', 'predicted_label', 'predicted_probability', 'concept_name', 'layer_name', 'positive_percentage', 'magnitude']  # Specify the desired order
+    df_merged = df_merged[desired_order]
+    
+    return df_merged
